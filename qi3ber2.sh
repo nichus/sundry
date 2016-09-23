e51bffd7f2288e480d175dc136a6faa5af680935
Orien Vandenbergh <orien.vandenbergh@lmco.com>
2016-09-23 17:23:28 -0600

    Fix the excess whitespace

--- qi3ber2.sh
+++ qi3ber2.sh
@@ -3,9 +3,9 @@ function qi3ber2() {
   if [ -n "$1" ]; then
     set_palette $1
   fi
-  
+
   RETCODE="\$( printf "%3s" \$CODE )"
-  
+
   UL="┌"
   UR="┐"
   MID="─"
@@ -13,19 +13,19 @@ function qi3ber2() {
   MR="├"
   LL="└"
   LR="┘"
-  
+
   LINEONE="${Theme[0]}${UL}${MID}${ML} ${Theme[3]}\u${Theme[1]}@${Theme[4]}\h${ResetColours}:${Theme[7]}${TTY}${Theme[0]} ${MR}${UR}"
   LINETWO="${Theme[0]}${MR}${MID}${ML} ${Theme[6]}$$${ResetColours}:${Theme[7]}${LOADAVG}${Theme[0]} @ ${Theme[8]}${DATE}${Theme[0]} ${MR}${ML} ${Theme[5]}\w${Theme[0]} ${MR}${UR}"
   LINETHR="${Theme[0]}${LL}${MID}${ML} ${Theme[2]}\! ${Theme[9]}${RETCODE}${Theme[0]} ${MR}${UR}"
-  
+
   PS1="${XTERM}$LINEONE \n$LINETWO \n$LINETHR \$${ResetColours} "
   export PS1
-  
+
   PS2="${Theme[0]}-${Theme[1]}*${Theme[2]}->${Theme[8]}\$${ResetColours} "
   export PS2
-  
+
   export OPROMPT=qi3ber2
-  
+
   unset LINEONE
   unset LINETWO
   unset LINETHR
